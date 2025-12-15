d57512d6674cdba304a0aa8ca46236e552b0cb49
Yasmin <ykasem84@gmail.com>
2025-12-15 18:38:01 +0200

    Add files via upload

---
+++ CAT question 1.py
@@ -0,0 +1,41 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#1
+#Descriptive statistics summarize and describe the characteristics of a dataset you have. They tell you what the data shows without making broader claim for examble The average test score in a class of 30 students is 82%.
+... #Inferential statistics use sample data to make predictions about a larger population. for example Based on surveying 500 voters, we estimate that 55% of all voters in the state support the candidate.
+... 
+... #2
+... #It is a range of values that is expected to include the true population value.
+... #When we say 95 percent confidence it means we trust that the real value is inside this range in most cases.
+... 
+... #3
+... #Correlation means two variables are related when one changes, the other change in a way, but causation means one variable directly causes the other to change.
+... #correlation doesn't prove causation, for example ice cream sales and drowning deaths are correlated both increase in summer, but ice cream doesn't cause drowning.
+... #4
+... #A probability distribution shows the values a random variable can take and how likely each value is.
+... #Discrete distributions deal with separate values like Binomial and Poisson.
+... #Continuous distributions deal with ranges like Normal and Exponential.
+... 
+... #5
+... #A population includes every individual or item you want to study.
+... #A sample is only a part taken from that population.
+... #You can calculate similar measures for both.
+... #Population measures use Greek symbols while sample measures use regular letters.
+... 
+... #6
+... #It is a bar chart used with continuous numerical data.
+... #The data is grouped into intervals called bins.
+... #It helps you understand the overall shape of the data.
+... #It shows where most values are centered.
+... #It shows how spread out the data is and helps spot unusual values.
+... 
+... #7
+... #Range is the difference between the maximum and minimum values in a dataset.
+... #IQR is the difference between the 75th percentile and 25th percentile.
+... 
+... #8
+... #A frequency distribution shows how often values appear in a dataset.
+... #It organizes the data into values or ranges with their counts.
+... #It makes large amounts of data easier to understand.
+... #It helps you notice patterns trends and the overall shape of the data.
+... 
