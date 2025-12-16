4c4ddaec4250bd277fcd2378f10be3b5a5b03742
Yasmin <ykasem84@gmail.com>
2025-12-16 15:37:52 +0200

    Add files via upload

---
+++ CAT question 3.py
@@ -0,0 +1,9 @@
+# shape of distribution
+#Left-skewed, Most values concentrate on the right side with a long tail extending to the left.
+
+#spread level
+#high spread, values are spread out over a wide range.
+#Variance/Standard Deviation change
+#If treated as a population, the variance & standard deviation will be smaller.
+#cause sample variance uses (n-1) as the divisor instead of N as in population.
+#while (n-1) < n, dividing by a smaller number gives a larger result for samples.
